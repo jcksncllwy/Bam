d903370595c1797541ed5874bded7252cd952394
jcksncllwy <jcksncllwy@gmail.com>
2012-07-14 12:31:41 -0700

    fixed chrome bug with navbar

--- js/script.js
+++ js/script.js
@@ -29,7 +29,9 @@ window.onload = function() {
 	for(var i=0; i<top_nav_items.length; i++){
 		top_nav_items[i].onclick = function(e){
 			for(var j=0; j<top_nav_items.length; j++){
-				top_nav_items[j].removeClass('active');
+				if(top_nav_items[j].hasClass('active')){
+					top_nav_items[j].removeClass('active');
+				}
 			}
 			if(e.target.hasClass('nav_item')){
 				e.target.addClass('active');
